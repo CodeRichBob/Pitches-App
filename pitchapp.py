397f0cec8262aef317b0b6e9a2f06489b39167bf
Robert <rgitonga.07@gmail.com>
2022-05-10 15:10:36 +0300

    ft:add register html file

--- pitchapp.py
+++ pitchapp.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '3a3b404948c4e3801e8a2a76859c4658'
 
 @app.route("/")
-@app.route("/main")
+@app.route("/home")
 def main():
     return render_template('main.html')
 
 
 @app.route("/About")
 def about():
-    return "<h1>Here you learn about us</h1>"
+    return render_template ('about.html', title = 'About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template ('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template ('login.html', title = 'Login', form=form)
 
 if __name__=='__main__':
     app.run(debug =True)
