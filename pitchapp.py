2a97692ad55b6e2cf0875c9815c2ad3a42715311
Robert <rgitonga.07@gmail.com>
2022-05-11 07:43:10 +0300

    validate login

--- pitchapp.py
+++ pitchapp.py
@@ -22,9 +22,10 @@ def register():
         return redirect(url_for('main'))
     return render_template ('register.html', title = 'Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
     return render_template ('login.html', title = 'Login', form=form)
 
 if __name__=='__main__':
