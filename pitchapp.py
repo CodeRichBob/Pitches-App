22bd2dbbfaf92086248ed60d5535ba0397a34a11
Robert <rgitonga.07@gmail.com>
2022-05-11 08:13:48 +0300

    install sqlalchemy and instantiate db

--- pitchapp.py
+++ pitchapp.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 
+
+
 app.config['SECRET_KEY'] = '3a3b404948c4e3801e8a2a76859c4658'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+
 
 @app.route("/")
 @app.route("/home")
