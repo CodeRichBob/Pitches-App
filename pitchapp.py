68ca2fc9a4119c6cf388c05d7e33421e08d998d7
Robert <rgitonga.07@gmail.com>
2022-05-11 12:26:16 +0300

    make modification to database

--- pitchapp.py
+++ pitchapp.py
@@ -11,26 +11,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
-class User (db.model):
+class User (db.Model):
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(20), unique = True, nullable = False )
     email = db.Column(db.String(120), unique = True, nullable = False )
-    profile_image = db.Column(db.String(20), nullable = False, default = default.jpg )
-    password = db.Column(db.string(60), nullable = False)
+    password = db.Column(db.String(60), nullable = False)
     posts = db.relationship('Post', backref = 'author', lazy = True)
 
 
-    def__repr__(self):
-        return f"User('{self.username}','{self.email}','{self.profile_image}')"
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}')"
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     title = db.Column(db.String(20), nullable = False)
-    date_posted = db.Column(db.Datetime, nullable = False, default = datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     content = db.Column(db.Text, nullable = False) 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
 
-    def__repr__(self):
+    def __repr__(self):
         return f"Post('{self.title}','{self.date_posted}')"
 
 
