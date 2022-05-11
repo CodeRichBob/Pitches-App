80a1ecf598786f813fd86b8cfc8d8e49c8d30e0c
Robert <rgitonga.07@gmail.com>
2022-05-11 07:49:25 +0300

    testing for user login

--- pitchapp.py
+++ pitchapp.py
@@ -26,6 +26,11 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        if form.email.data == 'user@email.com' and form.password.data == 'password'
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('main'))
+        else:
+            flash('Login unsuccessful, Please check username and password', 'danger')
     return render_template ('login.html', title = 'Login', form=form)
 
 if __name__=='__main__':
