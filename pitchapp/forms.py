30d9c9b5c15c15384807aa4be897080942baeb53
Robert <rgitonga.07@gmail.com>
2022-05-16 17:36:52 +0300

    add post file

--- pitchapp/forms.py
+++ pitchapp/forms.py
@@ -3,39 +3,37 @@ from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from pitchapp import *
+from pitchapp.models import User
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired(), Length(min=2, max = 20)])
-
-    email = StringField('Email', validators = [DataRequired(), Email()])
-
-    password = PasswordField('Password',validators = [DataRequired()] )
-
-    confirm_password = PasswordField('Password',validators = [DataRequired(),EqualTo('password')] )
-
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise validationError('That username is taken please choose another one')
+            raise ValidationError('That username is taken. Please choose a different one.')
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise validationError('That email is taken please choose another one')
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
-
-    email = StringField('Email', validators = [DataRequired(), Email()])
-
-    password = PasswordField('Password',validators = [DataRequired()] )
-
-    remember = BooleanField('Remember me')
-
-
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
@@ -57,8 +55,8 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
 
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
-
--- pitchapp/routes.py
+++ pitchapp/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, abort
 from pitchapp import app, db, bcrypt
 from pitchapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from pitchapp.models import User, Post
@@ -19,19 +19,19 @@ def main():
 def about():
     return render_template ('about.html', title = 'About')
 
-@app.route("/register", methods=['GET','POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('main'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email= form.email.data, password = hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'You account has been created, You are now able to login','success')
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
-    return render_template ('register.html', title = 'Register', form=form)
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
@@ -42,7 +42,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember = form.remember.data)
-            return redirect(url_for('main'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main'))
         else:
             flash('Login unsuccessful, Please check email and password', 'danger')
     return render_template ('login.html', title = 'Login', form=form)
@@ -52,7 +53,7 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('main'))
 
 
 def save_picture(form_picture):
